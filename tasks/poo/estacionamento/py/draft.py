1350a758c4e2c1ffff76c00d290ef64b4c2cc319
Riquelmetm <140022375+Riquelmetm@users.noreply.github.com>
2025-12-15 02:01:22 +0000

    estacionamento

--- tasks/poo/estacionamento/py/draft.py
+++ tasks/poo/estacionamento/py/draft.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 class Veiculo:
-    def __init__(self, id:str, entrada:int, tipo: str):
+    def __init__(self, id:str, entrada:int):
         self.id = id
         self.entrada = entrada
         self.tipo = tipo
@@ -13,8 +13,8 @@ class Veiculo:
         return f"{self.id},"
     
 class Bike(Veiculo):
-    def __init__(self, id: str, entrada: int, tipo: str):
-        super().__init__(id, entrada, tipo)
+    def __init__(self, id: str, entrada: int):
+        super().__init__(id, entrada)
         super().tipo = "Bike"
 
     def calcularValor(self, saida: int) -> float:
@@ -22,8 +22,8 @@ class Bike(Veiculo):
         return calculo
 
 class Moto(Veiculo):
-    def __init__(self, id: str, entrada: int, tipo: str):
-        super().__init__(id, entrada, tipo)
+    def __init__(self, id: str, entrada: int):
+        super().__init__(id, entrada)
         super().tipo = "Moto"
         
     def calcularValor(self, saida:int) -> float:
@@ -37,8 +37,8 @@ class Moto(Veiculo):
 
 
 class Carro(Veiculo):
-    def __init__(self, id: str, entrada: int, tipo: str):
-        super().__init__(id, entrada, tipo)
+    def __init__(self, id: str, entrada: int):
+        super().__init__(id, entrada)
         super().tipo = "Carro"
 
     def calcularValor(self, saida: int):
@@ -58,18 +58,26 @@ class Estacionamento:
         self.veiculo.append(veiculo)
     
     def pagar(self, id:str):
+        encontrado = None
         for i in self.veiculo:
-            if i == id:
-                
+            if i.id == id:
+                encontrado = i
+
+        if encontrado:
+            valor = encontrado.calcularValor(self.horaatual)
+            print(f"{encontrado.tipo} chegou {encontrado.entrada} saiu {self.horaatual}. Pagar R$ {valor}")
+            self.veiculo.remove(encontrado)
 
 
+    def tempo(self, tempo:int):
+        self.horaatual += tempo
 
     
     def __str__(self) -> str:
         if not self.veiculo:
-            return f"{self.horaatual}"
+            return ""
 
-        return f"{self.veiculo}"
+        return "\n".join([str(i) for i in self.veiculo])
     
 
 estacionamento = Estacionamento()
@@ -78,5 +86,27 @@ while True:
     a = input()
     args = a.split(" ")
 
-    if args[0] == "show":
+    if args[0] == "end":
+        break
+    
+    elif args[0] == "tempo":
+        estacionamento.tempo(int(args[1]))
 
+    elif args[0] == "show":
+        print(estacionamento)
+
+    elif args[0] == "estacionar":
+        tipo = args[1]
+        id = args[2]
+        if tipo == "bike":
+            bike = Bike(id, estacionamento.horaatual)
+            estacionamento.estacionar(bike)
+        elif tipo == "carro":
+            carro = Carro(id, estacionamento.horaatual)
+            estacionamento.estacionar(carro)
+        elif tipo == "moto":
+            moto = Moto(id, estacionamento.horaatual)
+            estacionamento.estacionar(moto)
+
+    elif args[0] == "pagar":
+        estacionamento.pagar(args[1])
